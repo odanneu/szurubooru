fb986297aa88a8996b77a48237e96217da806101
odanneu <95174765+odanneu@users.noreply.github.com>
2024-05-24 00:19:06 +0900

    Update top_navigation.js

--- client/js/models/top_navigation.js
+++ client/js/models/top_navigation.js
@@ -28,7 +28,7 @@ class TopNavigation extends events.EventTarget {
 
     get(key) {
         if (!this._keyToItem.has(key)) {
-            throw `An item with key ${key} does not exist.`;
+            throw `키 ${key} 에 해당하는 아이템이 존재하지 않습니다.`;
         }
         return this._keyToItem.get(key);
     }
@@ -36,7 +36,7 @@ class TopNavigation extends events.EventTarget {
     add(key, item) {
         item.key = key;
         if (this._keyToItem.has(key)) {
-            throw `An item with key ${key} was already added.`;
+            throw `키 ${key} 에 해당하는 아이템 이미 추가되어 있습니다.`;
         }
         this._keyToItem.set(key, item);
         this._items.push(item);
@@ -78,18 +78,18 @@ class TopNavigation extends events.EventTarget {
 
 function _makeTopNavigation() {
     const ret = new TopNavigation();
-    ret.add("home", new TopNavigationItem("H", "Home", ""));
-    ret.add("posts", new TopNavigationItem("P", "Posts", "posts"));
-    ret.add("upload", new TopNavigationItem("U", "Upload", "upload"));
-    ret.add("comments", new TopNavigationItem("C", "Comments", "comments"));
-    ret.add("tags", new TopNavigationItem("T", "Tags", "tags"));
-    ret.add("pools", new TopNavigationItem("O", "Pools", "pools"));
-    ret.add("users", new TopNavigationItem("S", "Users", "users"));
-    ret.add("account", new TopNavigationItem("A", "Account", "user/{me}"));
-    ret.add("register", new TopNavigationItem("R", "Register", "register"));
-    ret.add("login", new TopNavigationItem("L", "Log in", "login"));
-    ret.add("logout", new TopNavigationItem("O", "Logout", "logout"));
-    ret.add("help", new TopNavigationItem("E", "Help", "help"));
+    ret.add("home", new TopNavigationItem("H", "대문", ""));
+    ret.add("posts", new TopNavigationItem("P", "짤", "posts"));
+    ret.add("upload", new TopNavigationItem("U", "업로드", "upload"));
+    ret.add("comments", new TopNavigationItem("C", "댓글", "comments"));
+    ret.add("tags", new TopNavigationItem("T", "태그", "tags"));
+    ret.add("pools", new TopNavigationItem("O", "풀", "pools"));
+    ret.add("users", new TopNavigationItem("S", "사용자", "users"));
+    ret.add("account", new TopNavigationItem("A", "계정", "user/{me}"));
+    ret.add("register", new TopNavigationItem("R", "가입", "register"));
+    ret.add("login", new TopNavigationItem("L", "로그인", "login"));
+    ret.add("logout", new TopNavigationItem("O", "로그아웃", "logout"));
+    ret.add("help", new TopNavigationItem("E", "정보", "help"));
     ret.add(
         "settings",
         new TopNavigationItem(null, "<i class='fa fa-cog'></i>", "settings")
